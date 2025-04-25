fb22bf43e4a63509f0a3d5f23abe1106daa69cbf
Sharon Kendi <sharonkendi35@gmail.com>
2025-04-25 11:43:36 +0300

    Solves this lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,3 +1,4 @@
+// src/components/ProjectItem.js
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
@@ -6,10 +7,12 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech) => (
+          <span key={tech}>{tech}</span>
+        ))}
       </div>
     </div>
   );
 }
 
-export default ProjectItem;
+export default ProjectItem; // Make sure this is here
